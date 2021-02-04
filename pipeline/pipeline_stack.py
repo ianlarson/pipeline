8fc6d4d6d9ab49bee2911ad8e8b5b46287640f88
Larson <illarso@3c22fbe1e771.ant.amazon.com>
2021-02-03 20:09:30 -0700

    initialized cdk

---
+++ pipeline/pipeline_stack.py
@@ -0,0 +1,13 @@
+from aws_cdk import (aws_s3 as s3, core)
+
+
+class PipelineStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        bucket = s3.Bucket(self,
+                           "MyFirstBucket",
+                           versioned=True,
+                           public_read_access=True,
+                           removal_policy=core.RemovalPolicy.DESTROY)
