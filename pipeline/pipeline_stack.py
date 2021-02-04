077115784e336a4e051a8e1f5c4a0d230c3a69c8
Larson <illarso@3c22fbe1e771.ant.amazon.com>
2021-02-03 20:49:40 -0700

    pipeline added

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -1,4 +1,35 @@
 from aws_cdk import (aws_s3 as s3, core)
+from aws_cdk.core import Stack, StackProps, Construct, SecretValue
+from aws_cdk.pipelines import CdkPipeline, SimpleSynthAction
+
+import aws_cdk.aws_codepipeline as codepipeline
+import aws_cdk.aws_codepipeline_actions as codepipeline_actions
+
+
+class MyPipelineStack(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        source_artifact = codepipeline.Artifact()
+        cloud_assembly_artifact = codepipeline.Artifact()
+
+        pipeline = CdkPipeline(self, "Pipeline",
+            pipeline_name="MyAppPipeline",
+            cloud_assembly_artifact=cloud_assembly_artifact,
+            source_action=codepipeline_actions.GitHubSourceAction(
+                action_name="GitHub",
+                output=source_artifact,
+                oauth_token=SecretValue.secrets_manager("pipelineghsecret"),
+                trigger=codepipeline_actions.GitHubTrigger.POLL,
+                # Replace these with your actual GitHub project info
+                owner="ianlarson",
+                repo="pipeline"),
+            synth_action=SimpleSynthAction.standard_npm_synth(
+                source_artifact=source_artifact,
+                cloud_assembly_artifact=cloud_assembly_artifact
+            )
+        )
 
 
 class PipelineStack(core.Stack):
