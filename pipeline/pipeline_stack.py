9f58e57f6b322c27c352a0226c62d05c265b6019
Larson <illarso@3c22fbe1e771.ant.amazon.com>
2021-02-05 20:14:45 -0700

    cleared stuff out

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -2,35 +2,6 @@ from aws_cdk import (aws_s3 as s3, core, aws_ec2 as ec2)
 from aws_cdk.core import Stack, StackProps, Construct, SecretValue
 from aws_cdk.pipelines import CdkPipeline, SimpleSynthAction
 
-import aws_cdk.aws_codepipeline as codepipeline
-import aws_cdk.aws_codepipeline_actions as codepipeline_actions
-
-
-class MyPipelineStack(Stack):
-
-    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
-
-        source_artifact = codepipeline.Artifact()
-        cloud_assembly_artifact = codepipeline.Artifact()
-
-        pipeline = CdkPipeline(self, "Pipeline",
-            pipeline_name="MyAppPipeline",
-            cloud_assembly_artifact=cloud_assembly_artifact,
-            source_action=codepipeline_actions.GitHubSourceAction(
-                action_name="GitHub",
-                output=source_artifact,
-                oauth_token=SecretValue.secrets_manager("pipelineghsecret"),
-                trigger=codepipeline_actions.GitHubTrigger.POLL,
-                # Replace these with your actual GitHub project info
-                owner="ianlarson",
-                repo="pipeline"),
-            synth_action=SimpleSynthAction.standard_npm_synth(
-                source_artifact=source_artifact,
-                cloud_assembly_artifact=cloud_assembly_artifact
-            )
-        )
-
 
 class PipelineStack(core.Stack):
 
@@ -43,6 +14,6 @@ class PipelineStack(core.Stack):
                            public_read_access=False,
                            removal_policy=core.RemovalPolicy.DESTROY)
 
-        vpc = ec2.Vpc(self, "TheVPC",
-                      cidr="10.1.0.0/16")
+        # vpc = ec2.Vpc(self, "TheVPC",
+        #               cidr="10.1.0.0/16")
 
