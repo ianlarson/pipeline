fddf9210d833f5b003121492da2be068b4f1f7c0
Ian Larson <illarso@amazon.com>
2021-06-24 10:23:44 -0700

    changed bucket name

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -10,6 +10,7 @@ class PipelineStack(core.Stack):
 
         bucket = s3.Bucket(self,
                            "illarsoMyFirstBucket12341234",
+                           bucket_name='illarsoMyFirstBucket12341234',
                            versioned=False,
                            public_read_access=False,
                            removal_policy=core.RemovalPolicy.DESTROY)
