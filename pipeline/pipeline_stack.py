670eb0f78d890a5f813c1d545c2691041adf14f9
Ian Larson <illarso@amazon.com>
2022-04-06 08:14:54 -0700

    test

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -8,13 +8,12 @@ class PipelineStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # bucket = s3.Bucket(self,
-        #                    "illarsoMyFirstBucket12341234",
-        #                    bucket_name='illarsomyfirstbucket12',
-        #                    versioned=False,
-        #                    public_read_access=False,
-        #                    removal_policy=core.RemovalPolicy.DESTROY)
+        bucket = s3.Bucket(self,
+                           "illarsoMyFirstBucket12341234",
+                           bucket_name='illarsomyfirstbucket12',
+                           versioned=False,
+                           public_read_access=False,
+                           removal_policy=core.RemovalPolicy.DESTROY)
 
-        # vpc = ec2.Vpc(self, "TheVPC",
-        #               cidr="10.1.0.0/16")
-
+        vpc = ec2.Vpc(self, "TheVPC",
+                      cidr="10.1.0.0/16")
